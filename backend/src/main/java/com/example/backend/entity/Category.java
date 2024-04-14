d3cfe39932fcc01055f47d27949ecc0b9542790d
Kei Murakami <buncho514@icloud.com>
2024-04-14 10:53:11 +0900

    バージョンアップ

--- backend/src/main/java/com/example/backend/entity/Category.java
+++ backend/src/main/java/com/example/backend/entity/Category.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
